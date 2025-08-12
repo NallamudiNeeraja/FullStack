bb6d3d2a18985b1b2dfa95c6b1e601c847aaf7b5
Neeraja <neerajanallamudi@gmail.com>
2025-08-12 16:10:12 +0530

    Dev1 performing UPIPayment

---
+++ MavenProj2/src/main/java/com/nt/pack1/UIPayment.java
@@ -0,0 +1,8 @@
+package com.nt.pack1;
+
+public class UIPayment 
+{
+public String doPaymentUsingPhonepe(long phoneno,double amount) {
+	return amount+"payment is happening from the phone number"+phoneno;
+}
+}
