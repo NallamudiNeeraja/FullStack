11950344f29065d5bec4d375311d690ff70ea90e
Neeraja <neerajanallamudi@gmail.com>
2025-08-12 16:25:23 +0530

    done

--- MavenProj2/src/main/java/com/nt/pack1/UIPayment.java
+++ MavenProj2/src/main/java/com/nt/pack1/UIPayment.java
@@ -3,6 +3,6 @@ package com.nt.pack1;
 public class UIPayment 
 {
 public String doPaymentUsingPhonepe(long phoneno,double amount) {
-	return amount+"payment is happening from the phone number"+phoneno;
+	return amount+"payment is happening from the phone number";
 }
 }
