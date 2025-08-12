6668a2be88943b0275e423e741de8c8c5fe2f4ba
Neeraja <neerajanallamudi@gmail.com>
2025-08-12 16:46:49 +0530

    Added mobile

--- MavenProj2/src/main/java/com/nt/pack1/UIPayment.java
+++ MavenProj2/src/main/java/com/nt/pack1/UIPayment.java
@@ -5,4 +5,7 @@ public class UIPayment
 public String doPaymentUsingPhonepe(long phoneno,double amount) {
 	return amount+"payment is happening from the phone number";
 }
+public String doPaymentUsingMobile(long phoneno,double amount) {
+	return amount+"payment is happening from the Mobile";
+}
 }
